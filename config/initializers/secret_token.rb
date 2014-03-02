de9fc79ceccba603d1c39ac925efe92352c2c2db
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-02 14:00:13 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CircleMusic::Application.config.secret_key_base = '6919b0a1fc923420b8548320ccc8e5fa16c3de38e3a8e6ebbac1a18fd3d2486297661a80a057e9a556f42ee0bcda6ce40c3600b881af55f2912321ad7fa8d39d'
