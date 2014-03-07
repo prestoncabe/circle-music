2055c48381adc6fc7ab0ff2153f65b16296edf5c
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-07 10:27:09 -0500

    signed in users don't see sign up link

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Sign up' do
-  scenario 'as a visitor' do
+  scenario 'visitors can sign up from the homepage' do
     visit root_path
 
     click_link 'Sign up'
@@ -15,4 +15,17 @@ feature 'Sign up' do
     expect(page).to have_css '[data-role="description"]'
   end
 
+  scenario 'signed in users do not see sign up link' do
+    visit root_path
+
+    click_link 'Sign up'
+
+    fill_in 'Email', with: 'person@example.com'
+    fill_in 'Password', with: '1234'
+
+    click_button 'Sign up'
+
+    expect(page).to have_css '[data-role="description"]'
+    expect(page).not_to have_link 'Sign up'
+  end
 end
