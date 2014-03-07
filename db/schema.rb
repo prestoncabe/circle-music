f507260c08afd0e1003157e8b10e89772cc20a8a
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-07 16:22:06 -0500

    fix CreateSongs migration to include timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,9 @@
 ActiveRecord::Schema.define(version: 20140307200647) do
 
   create_table "songs", force: true do |t|
-    t.string "title"
+    t.string   "title",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: true do |t|
