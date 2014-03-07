28560b15d043114d5f68c31c305fa551028551b8
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-07 10:36:49 -0500

    refactor sign up spec to use single sign_up method

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,12 +4,7 @@ feature 'Sign up' do
   scenario 'visitors can sign up from the homepage' do
     visit root_path
 
-    click_link 'Sign up'
-
-    fill_in 'Email', with: 'person@example.com'
-    fill_in 'Password', with: '1234'
-
-    click_button 'Sign up'
+    sign_up
 
     expect(page).to have_title 'CircleMusic'
     expect(page).to have_css '[data-role="description"]'
@@ -18,14 +13,20 @@ feature 'Sign up' do
   scenario 'signed in users do not see sign up link' do
     visit root_path
 
+    sign_up
+
+    expect(page).to have_css '[data-role="description"]'
+    expect(page).not_to have_link 'Sign up'
+  end
+
+  def sign_up
+    visit root_path
+
     click_link 'Sign up'
 
     fill_in 'Email', with: 'person@example.com'
     fill_in 'Password', with: '1234'
 
     click_button 'Sign up'
-
-    expect(page).to have_css '[data-role="description"]'
-    expect(page).not_to have_link 'Sign up'
   end
 end
