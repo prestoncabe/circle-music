2b8fa796267bf89700be66335198025092263689
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-11 13:28:32 -0400

    refactor view songs spec for DRYness and readability

--- spec/features/view_songs_spec.rb
+++ spec/features/view_songs_spec.rb
@@ -6,44 +6,62 @@ feature 'View songs' do
   end
 
   scenario 'as a visitor to songs index page' do
-
     visit songs_path
 
-    expect(page).to have_css 'li.song', text: 'Come Thou Fount'
-    expect(page).to have_css 'li.song', text: 'Lord I Lift'
+    visitor_sees_list_of_songs
   end
 
   scenario 'as a user on songs index page' do
-
     sign_up
 
     visit songs_path
 
-    expect(page).to have_css 'li.song', text: 'Come Thou Fount'
-    expect(page).to have_css 'li.song', text: 'Lord I Lift'
+    user_sees_list_of_songs
   end
 
   scenario 'as a visitor to songs show page' do
-
     visit songs_path
-    click_link 'Come Thou Fount'
 
-    expect(page).to have_content 'Come Thou Fount'
-    expect(page).not_to have_content 'Lord I Lift'
+    click_song_title_link
+
+    visitor_sees_song_show_page
   end
 
   scenario 'as a user on the songs show page' do
+    sign_up
 
     visit songs_path
-    click_link 'Lord I Lift'
 
-    expect(page).to have_content 'Lord I Lift'
-    expect(page).not_to have_content 'Come Thou Fount'
+    click_song_title_link
+
+    user_sees_song_show_page
   end
 
   def create_songs(song_titles)
     song_titles.each do |song_title|
       create(:song, title: song_title)
     end
+  end
+
+  def user_sees_list_of_songs
+    expect(page).to have_css 'li.song', text: 'Come Thou Fount'
+    expect(page).to have_css 'li.song', text: 'Lord I Lift'
+  end
+
+  def visitor_sees_list_of_songs
+    user_sees_list_of_songs
+  end
+
+  def click_song_title_link
+    click_link 'Come Thou Fount'
+  end
+
+  def visitor_sees_song_show_page
+    expect(page).to have_content 'Come Thou Fount'
+    expect(page).not_to have_content 'Lord I Lift'
+  end
+
+  def user_sees_song_show_page
+    visitor_sees_song_show_page
   end
 end
