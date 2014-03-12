74b6e831d11e59e96fd5bf28d2d403a5278067ca
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-12 16:44:34 -0400

    admin users can delete songs from songs index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140307200647) do
+ActiveRecord::Schema.define(version: 20140312181602) do
 
   create_table "songs", force: true do |t|
     t.string   "title",      null: false
@@ -20,12 +20,13 @@ ActiveRecord::Schema.define(version: 20140307200647) do
   end
 
   create_table "users", force: true do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.string   "email",                                          null: false
+    t.string   "encrypted_password", limit: 128,                 null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.string   "remember_token",     limit: 128,                 null: false
+    t.boolean  "admin",                          default: false, null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
