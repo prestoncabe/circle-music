771d3c8dc4794dd2fa7f24b1be0d9a0d1edbdc19
Preston Cabe <cabep@ldc.upenn.edu>
2014-03-22 20:19:50 -0400

    refactor manage songs spec for clarity

--- spec/features/manage_songs_spec.rb
+++ spec/features/manage_songs_spec.rb
@@ -21,29 +21,28 @@ feature 'Manage songs' do
     user_sees_song_titled 'Come Thou Fount'
   end
 
-  scenario 'guest cannot add songs from homepage' do
+  scenario 'guest cannot add song from homepage' do
     visit root_path
 
     expect(page).not_to have_link 'Add a song'
   end
 
-  scenario 'guest cannot add songs via new_song_path' do
+  scenario 'guest cannot add song via new_song_path' do
     visit new_song_path
 
     expect(current_path).to eq sign_in_path
   end
 
   scenario 'delete song as admin from songs index page' do
-    create(:song, title: 'Come Thou Fount')
-    create(:song, title: 'Lord I Lift')
+    song_to_delete = create(:song, title: 'Come Thou Fount')
+    song_to_keep = create(:song, title: 'Lord I Lift')
     admin_user = create(:user, email: 'admin@example.com', admin: true)
 
     visit songs_path as: admin_user
-    within "li.song:contains('Lord I Lift')" do
-      click_link 'delete'
-    end
+    click_delete_for_song(song_to_delete)
 
-    expect(page).not_to have_content 'Lord I Lift'
+    user_does_not_see_song(song_to_delete)
+    user_sees_song(song_to_keep)
   end
 
   scenario 'non-admins cannot delete songs' do
@@ -53,7 +52,7 @@ feature 'Manage songs' do
 
     visit songs_path as: non_admin_user
 
-    expect(page).not_to have_content 'delete'
+    expect(page).not_to have_text 'delete'
   end
 
   def add_song_with_title(title)
@@ -62,8 +61,21 @@ feature 'Manage songs' do
     click_button 'Create Song'
   end
 
+  def click_delete_for_song(song)
+    within "li.song:contains('#{song.title}')" do
+      click_link 'delete'
+    end
+  end
   def user_sees_song_titled(title)
-    expect(page).to have_css 'li.song', text: 'Come Thou Fount'
+    expect(page).to have_css 'li.song', text: title
+  end
+
+  def user_sees_song(song)
+    expect(page).to have_css 'li.song', text: song.title
+  end
+
+  def user_does_not_see_song(song)
+    expect(page).not_to have_css 'li.song', text: song.title
   end
 end
 
